3fc03cdfc96131af21ab8b4b65e758feeae08bdb
rafaelvpds <33781893+rafaelvpds@users.noreply.github.com>
2023-07-17 14:53:12 -0300

    first commit - created project

---
+++ src/main/java/com/rvsystem/courseMongodb/CourseMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.rvsystem.courseMongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseMongodbApplication.class, args);
+	}
+
+}
