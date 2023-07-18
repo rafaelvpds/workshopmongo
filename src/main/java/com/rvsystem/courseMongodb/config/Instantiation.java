d42c40121cb0054eb0b602f11287591ba6300723
rafaelvpds <33781893+rafaelvpds@users.noreply.github.com>
2023-07-18 16:50:12 -0300

    Criando entity Post com User aninhado

--- src/main/java/com/rvsystem/courseMongodb/config/Instantiation.java
+++ src/main/java/com/rvsystem/courseMongodb/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.rvsystem.courseMongodb.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.rvsystem.courseMongodb.domain.Post;
 import com.rvsystem.courseMongodb.domain.User;
+import com.rvsystem.courseMongodb.repository.PostRepository;
 import com.rvsystem.courseMongodb.repository.UserRepository;
 
 //Para o Spring entender que e uma configuração
@@ -15,16 +19,28 @@ public class Instantiation implements CommandLineRunner {
 	@Autowired
 	private UserRepository userRepository;
 
+	@Autowired
+	private PostRepository postRepository;
+
 	@Override
 	public void run(String... args) throws Exception {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 
 		userRepository.deleteAll();
+		postRepository.deleteAll();
+
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem!", "Vou viajar para São Paulo. Abraços!",
+				maria);
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+
+		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
 }
---
+++ src/main/java/com/rvsystem/courseMongodb/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.rvsystem.courseMongodb.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.rvsystem.courseMongodb.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
