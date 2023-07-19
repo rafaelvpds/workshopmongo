528df67d8c0c86e73d0d370eacefaed466bfa76c
rafaelvpds <33781893+rafaelvpds@users.noreply.github.com>
2023-07-19 14:42:18 -0300

    Consulta simples com query methods

--- src/main/java/com/rvsystem/courseMongodb/services/PostService.java
+++ src/main/java/com/rvsystem/courseMongodb/services/PostService.java
@@ -1,5 +1,6 @@
 package com.rvsystem.courseMongodb.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +19,10 @@ public class PostService {
 		Optional<Post> obj = postRepository.findById(id);
 
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Id not found " + id));
+	}
+
+	public List<Post> findByTitle(String text) {
+		return postRepository.findByTitleContainingIgnoreCase(text);
 	}
 
 }
