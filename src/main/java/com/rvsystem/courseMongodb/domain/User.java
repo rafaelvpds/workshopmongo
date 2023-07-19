ea835ec9f7f0650552e807118a3b781a64619407
rafaelvpds <33781893+rafaelvpds@users.noreply.github.com>
2023-07-19 10:29:38 -0300

    Referenciando os posts dos usuarios

--- src/main/java/com/rvsystem/courseMongodb/domain/User.java
+++ src/main/java/com/rvsystem/courseMongodb/domain/User.java
@@ -1,9 +1,12 @@
 package com.rvsystem.courseMongodb.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
@@ -14,6 +17,11 @@ public class User implements Serializable {
 	private String id;
 	private String name;
 	private String email;
+
+	@DBRef(lazy = true)
+	// nao queremos carregar altomaticamente os posts, quando queremos carregar um
+	// usuario
+	private List<Post> posts = new ArrayList<>();
 
 	public User() {
 	}
@@ -46,6 +54,14 @@ public class User implements Serializable {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
 	}
 
 	@Override
